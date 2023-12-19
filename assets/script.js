e75fb331beacec5987220b1e33105aed9f1b3022
EYUP <jobmoonday2@gmail.com>
2023-12-19 19:39:41 +0000

    UI visual improvements have been done

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,7 @@ function addButton(buttonText) {
 }
 
 $("#search-button").on("click", function() {
-    var searchWord = $("#search-input").val() || "london";
+    var searchWord = $("#search-input").val() || "sheffield";
     if(localStorage.getItem("cities") !== null) {
         cities = JSON.parse(localStorage.getItem("cities"));
     }
@@ -78,10 +78,10 @@ function setCurrentData(respCurrent) {
     var day= moment().get("date");
     var month = moment().get("month") + 1;
     var year = moment().get("year")
-    $("#titleToday").text("TODAY: " + "   " + day + "_" + month + "_" + year +"");
+    $("#titleToday").text("TODAY: " + "   " + day + " / " + month + " / " + year +"");
     $("#title").text(respCurrent.name);
-    $("#current-temp").text("Temp: " + respCurrent.main.temp + " °C");
-    $("#current-wind").text("Wind: " + respCurrent.wind.speed + " Km/ph");
+    $("#current-temp").text("Temperature: " + respCurrent.main.temp + " °C");
+    $("#current-wind").text("Wind: " + respCurrent.wind.speed + " Km/h");
     $("#current-hum").text("Humidity: " + respCurrent.main.humidity + "%");
 
 }
@@ -91,31 +91,31 @@ function setForecastData(respFuture) {
             $("#icon1").attr("src",iconurl1);
             $("#title1").text((respFuture.list[3].dt_txt.split(" "))[0]);
             $("#temp1").text("Temp: " + respFuture.list[3].main.temp + " °C");
-            $("#wind1").text("Wind: " + respFuture.list[3].wind.speed + " Km/ph");
+            $("#wind1").text("Wind: " + respFuture.list[3].wind.speed + " Km/h");
             $("#hum1").text("Humidity: " + respFuture.list[3].main.humidity + "%");
     var iconurl2 = "https://openweathermap.org/img/w/" + respFuture.list[11].weather[0].icon+ ".png";
             $("#icon2").attr("src",iconurl2);
             $("#title2").text((respFuture.list[11].dt_txt.split(" "))[0]);
             $("#temp2").text("Temp: " + respFuture.list[11].main.temp + " °C");
-            $("#wind2").text("Wind: " + respFuture.list[11].wind.speed + " Km/ph");
+            $("#wind2").text("Wind: " + respFuture.list[11].wind.speed + " Km/h");
             $("#hum2").text("Humidity: " + respFuture.list[11].main.humidity + "%");
     var iconurl3 = "https://openweathermap.org/img/w/" + respFuture.list[19].weather[0].icon+ ".png";
             $("#icon3").attr("src",iconurl3);
             $("#title3").text((respFuture.list[19].dt_txt.split(" "))[0]);
             $("#temp3").text("Temp: " + respFuture.list[19].main.temp + " °C");
-            $("#wind3").text("Wind: " + respFuture.list[19].wind.speed + " Km/ph");
+            $("#wind3").text("Wind: " + respFuture.list[19].wind.speed + " Km/h");
             $("#hum3").text("Humidity: " + respFuture.list[19].main.humidity + "%");
     var iconurl4 = "https://openweathermap.org/img/w/" + respFuture.list[27].weather[0].icon+ ".png";
             $("#icon4").attr("src",iconurl4);
             $("#title4").text((respFuture.list[27].dt_txt.split(" "))[0]);
             $("#temp4").text("Temp: " + respFuture.list[27].main.temp + " °C");
-            $("#wind4").text("Wind: " + respFuture.list[27].wind.speed + " Km/ph");
+            $("#wind4").text("Wind: " + respFuture.list[27].wind.speed + " Km/h");
             $("#hum4").text("Humidity: " + respFuture.list[27].main.humidity + "%");
     var iconurl5 = "https://openweathermap.org/img/w/" + respFuture.list[35].weather[0].icon+ ".png";
             $("#icon5").attr("src",iconurl5);
             $("#title5").text((respFuture.list[35].dt_txt.split(" "))[0]);
             $("#temp5").text("Temp: " + respFuture.list[35].main.temp + " °C");
-            $("#wind5").text("Wind: " + respFuture.list[35].wind.speed + " Km/ph");
+            $("#wind5").text("Wind: " + respFuture.list[35].wind.speed + " Km/h");
             $("#hum5").text("Humidity: " + respFuture.list[35].main.humidity + "%");
 }
 
